b39de15012aa5e0f0e84d9d8274970c6c7910dda
Андрей <dusha22075@gmail.com>
2024-02-17 15:19:18 +0300

    Добавьте файлы проекта.

---
+++ DZ_25.02.2024_7/Program.cs
@@ -0,0 +1,33 @@
+﻿// Пользователь вводит с клавиатуры два числа. Нужно показать все четные числа в указанном диапазоне.
+// Если границы диапазона указаны неправильно требуется произвести нормализацию границ.
+// Например, пользователь ввел 20 и 11, требуется нормализация,
+// после которой начало диапазона станет равно 11, а конец 20.
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите два числа для определения диапазона:");
+
+        Console.Write("Первое число: ");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Второе число: ");
+        int number2 = int.Parse(Console.ReadLine());
+
+        // Проверка и нормализация границ диапазона
+        int start = Math.Min(number1, number2);
+        int end = Math.Max(number1, number2);
+
+        Console.WriteLine($"Нормализованный диапазон: [{start}, {end}]");
+
+        Console.WriteLine("Четные числа в указанном диапазоне:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
